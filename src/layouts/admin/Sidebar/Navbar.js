d59e23c98d620539f22d9c8c66af54e444536fcb
Justice Opara <justiceopara33@gmail.com>
2026-03-06 13:25:12 +0100

    feat: Update admin sidebar header and navbar components, triggering a new build.

--- src/layouts/admin/Sidebar/Navbar.js
+++ src/layouts/admin/Sidebar/Navbar.js
@@ -1,4 +1,3 @@
-import { Skeleton } from "@chakra-ui/skeleton";
 import { AiOutlinePoweroff } from "react-icons/ai";
 import { Button, Heading } from "../../../components";
 import { useApp } from "../../../contexts";
@@ -6,13 +5,11 @@ import { links, settingsLinks, superAdminSettingsLinks } from "./links";
 import SidebarLink from "./SidebarLink";
 import {
   IconButton,
-  Avatar,
   Box,
   CloseButton,
   Flex,
   Stack,
   useColorModeValue,
-  Link,
   Drawer,
   DrawerContent,
   DrawerOverlay,
@@ -102,44 +99,7 @@ export const SidebarContent = ({ onClose, ...rest }) => {
             </Heading>
           </Box>
         ) : (
-          <Box padding={5}>
-            <Stack
-              as="header"
-              spacing={2}
-              justifyContent="center"
-              alignItems="center"
-              borderBottom="1px"
-              borderColor="gray.200"
-              height="200px"
-              marginBottom={5}
-              paddingBottom={5}
-            >
-              {!state.user ? (
-                <Skeleton rounded="full" boxSize="100px" />
-              ) : (
-                <Avatar
-                  name={state.user?.firstName + " " + state.user?.lastName}
-                  borderRadius="100%"
-                  width="100px"
-                  height="100px"
-                  src={state.user?.profilePics}
-                />
-              )}
-
-              {state.user && (
-                <>
-                  <Link href={`/admin/users/details/${state.user.id}/profile`}>
-                    <Text fontSize="heading.h3">
-                      {state.user.firstName || "NotSet"} {state.user.lastName}
-                    </Text>
-                  </Link>
-
-                  <Text color="gray.500" textTransform="capitalize">
-                    {getOneMetadata("userRoles", state.user.userRoleId)?.name}
-                  </Text>
-                </>
-              )}
-            </Stack>
+          <Box padding={0}>
           </Box>
         )}
 
@@ -148,14 +108,14 @@ export const SidebarContent = ({ onClose, ...rest }) => {
             {isSettingsPage
               ? isSuperAdmin
                 ? superAdminSettingsLinks.map((link) => (
-                    <SidebarLink key={link.text} link={link} />
-                  ))
+                  <SidebarLink key={link.text} link={link} />
+                ))
                 : settingsLinks.map((link) => (
-                    <SidebarLink key={link.text} link={link} />
-                  ))
+                  <SidebarLink key={link.text} link={link} />
+                ))
               : links.map((link) => (
-                  <SidebarLink key={link.text} link={link} onClick={onClose} />
-                ))}
+                <SidebarLink key={link.text} link={link} onClick={onClose} />
+              ))}
           </Stack>
         </Box>
         <Box padding={5} mt="auto">
